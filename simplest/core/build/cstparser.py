f7dc9b6126c51c8a21d498f588bd0e1b6c4991af
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-02-16 04:33:42 -0600

    Add configuration methods to StreamlitComponentParser for enhanced state management

--- simplest/core/build/cstparser.py
+++ simplest/core/build/cstparser.py
@@ -11,6 +11,60 @@ class StreamlitComponentParser:
         self.component = component
         self.args = args
         self.kwargs = kwargs
+        self._stateful = False
+        self._fatal = True
+        self._errhandler = None
+        self._strict = True
+        self._autoconfig = True
+    
+    @property
+    def config(self):
+        return {
+            "stateful": self._stateful,
+            "fatal": self._fatal,
+            "errhandler": self._errhandler,
+            "strict": self._strict,
+        }
+    
+
+    def set_config(self, **kwargs):
+        if not kwargs:
+            return self.config
+        
+        if "stateful" in kwargs:
+            self.set_stateful(kwargs["stateful"])
+        if "fatal" in kwargs:
+            self.set_fatal(kwargs["fatal"])
+        if "errhandler" in kwargs:
+            self.set_errhandler(kwargs["errhandler"])
+        if "strict" in kwargs:
+            self.set_strict(kwargs["strict"])
+        return self.config
+
+    def set_stateful(self, stateful: bool) -> "StreamlitComponentParser":
+        self._stateful = stateful
+        return self
+    
+    def set_fatal(self, fatal: bool) -> "StreamlitComponentParser":
+        self._fatal = fatal
+        return self
+    
+    def set_errhandler(self, errhandler: Callable[[Exception], Union[NoReturn, bool]]) -> "StreamlitComponentParser":
+        self._errhandler = errhandler
+        return self
+    
+    def set_strict(self, strict: bool) -> "StreamlitComponentParser":
+        self._strict = strict
+        return self
+
+    def set_autoconfig(self, autoconfig: bool) -> "StreamlitComponentParser":
+        self._autoconfig = autoconfig
+        return self
+    
+    
+    def is_autoconfig(self) -> bool:
+        return self._autoconfig
+
 
     def parse(
         self,
@@ -19,6 +73,12 @@ class StreamlitComponentParser:
         errhandler: Callable[[Exception], Union[NoReturn, bool]] = None,
         strict: bool = True,
     ) -> Union[IElement, VElement]:
+        if self.is_autoconfig():
+            stateful = self.config["stateful"]
+            fatal = self.config["fatal"]
+            errhandler = self.config["errhandler"]
+            strict = self.config["strict"]
+    
         if stateful:
             comp = IElement(*self.args, **self.kwargs)
             comp._set_base_component(self.component).set_errhandler(
@@ -31,3 +91,11 @@ class StreamlitComponentParser:
             ).set_fatal(fatal)
 
         return comp
+
+    def __call__(self, *args, **kwargs):
+        if not args and not kwargs:
+            com = self.parse()
+        else:
+            com = self.parse(*args, **kwargs)
+        
+        return com()
