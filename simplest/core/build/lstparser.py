ea4bfff80c41a131c50d2d112c41de3205984eb4
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-02-16 08:31:41 -0600

    Add detailed docstrings to StreamlitLayoutParser methods for improved clarity

--- simplest/core/build/lstparser.py
+++ simplest/core/build/lstparser.py
@@ -4,6 +4,14 @@ from ..components.container import Container
 
 class StreamlitLayoutParser:
     def __init__(self, container: Callable[..., Any], *args, **kwargs):
+        """
+        Initialize the parser with a container and optional arguments.
+
+        Args:
+            container (Callable[..., Any]): A callable that will be used as the container.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         self.container = container
         self.args = args
         self.kwargs = kwargs
@@ -16,6 +24,19 @@ class StreamlitLayoutParser:
         layers: Dict[Union[int, str], List[Callable[..., Any]]] = None,
         order: List[Union[int, str]] = None,
     ) -> Container:
+        """
+        Parses the input and constructs a Container object with the specified configuration.
+
+        Args:
+            fatal (bool): If True, the parser will raise exceptions on errors. Defaults to True.
+            errhandler (Callable[[Exception], Union[NoReturn, bool]], optional): A custom error handler function. Defaults to None.
+            column_based (bool): If True, the parser will operate in column-based mode. Defaults to False.
+            layers (Dict[Union[int, str], List[Callable[..., Any]]], optional): A dictionary mapping layer identifiers to lists of callables. Defaults to None.
+            order (List[Union[int, str]], optional): A list specifying the order of layers. Defaults to None.
+
+        Returns:
+            Container: A Container object configured with the specified parameters.
+        """
         comp = Container(*self.args, **self.kwargs)
         comp._set_base_component(self.container).set_errhandler(errhandler).set_fatal(
             fatal
