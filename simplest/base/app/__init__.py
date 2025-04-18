36afb5efaa566c47509761577bbd8cf583b981f9
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-04-18 05:20:51 -0600

    Add __init__.py for component exports including AppPage, AppFragment, and AppDialog

---
+++ simplest/base/app/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .singleapp import AppPage
+from .fragment import AppFragment
+from .dialog import AppDialog
+
+
+__all__ = [
+    "AppPage",
+    "AppFragment",
+    "AppDialog",
+]
