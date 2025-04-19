4a9bff43d5ffc6e54d92d4814c5e33dc043ca02c
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-04-19 09:02:25 -0600

    Add BaseRepresentation and BaseStandard classes for representation management

---
+++ st_simple/config/base/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .representation import BaseRepresentation
+from .standard import BaseStandard
+
+__all__ = [
+    "BaseRepresentation",
+    "BaseStandard",
+]
