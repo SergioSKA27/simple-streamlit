d17fb1480943b0d78945111a3e51a868c9f8f293
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-04-19 08:54:30 -0600

    Add base classes for layoutable, renderable, and stateful components

---
+++ st_simple/core/base/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .layoutable import Layoutable
+from .renderable import Renderable
+from .stateful import Stateful
+
+_all__ = [
+    "Layoutable",
+    "Renderable",
+    "Stateful",
+]
