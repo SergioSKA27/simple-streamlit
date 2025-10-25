f3361591daff0eec896484340f7ad95b7e33071d
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-10-25 06:55:45 -0600

    Add Container, IElement, and VElement classes with serialization and rendering capabilities

---
+++ declarative_streamlit/core/components/container.py
@@ -0,0 +1,63 @@
+from typing import List, Dict, Any, Callable, NoReturn, Union, Literal
+from ..base.renderable import Renderable
+from ..base.composable import Composable
+
+
+class Container(Renderable, Composable):
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of the container component.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Attributes:
+            _base_component (Callable[..., Any]): The base component of the container.
+            _top_render (bool): Flag indicating if this is the top render.
+        """
+        Renderable.__init__(self, *args, **kwargs)
+        Composable.__init__(self)
+        self._base_component = None  # type: Callable[..., Any]
+        self._top_render = False  # type: bool
+        self.schema.set_body_name("__container__")
+
+    def render(self, *args, **kwargs):
+        """
+        Renders the base component with the provided arguments and keyword arguments.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        self.lrender(self._base_component, *args, **kwargs)
+
+    # override the __str__ method
+    def __str__(self):
+        """
+        Return a string representation of the Container object.
+
+        The string representation includes the name of the base component and the layers of the container.
+
+        Returns:
+            str: A string in the format "Container(<base_component_name>): <layers>".
+        """
+        return f"Container({self._base_component.__name__}): {self.schema}"
+
+    def serialize(self):
+        """
+        Serializes the container object into a dictionary format.
+        Returns:
+            dict: A dictionary containing the serialized container object.
+        """
+
+        if self._base_component is not None:
+            self.schema.set_body_name("__children__")
+        return {
+            "__component__": self._base_component.__name__,
+            "__args__": {
+                "args": self.args,
+                "kwargs": self.kwargs,
+            },
+            "__type__": "Container",
+        }
